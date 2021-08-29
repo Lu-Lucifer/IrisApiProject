bbedd7f28abcd190e100cb69d45551f2095b3215
snwolyg <569616226@qq.com>
2021-08-29 16:19:16 +0800

    增加初始化模块

---
+++ modules/init_db/index.go
@@ -0,0 +1,14 @@
+package init_db
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/snowlyg/iris-admin/server/module"
+)
+
+// Party
+func Party() module.WebModule {
+	handler := func(index iris.Party) {
+		index.Post("/initdb", Init)
+	}
+	return module.NewModule("/init", handler)
+}
