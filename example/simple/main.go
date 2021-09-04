b860d7f1b381ed973db75440c52950379b154d43
snwolyg <569616226@qq.com>
2021-09-04 17:16:25 +0800

    完善模块数据填充

--- example/simple/main.go
+++ example/simple/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"github.com/snowlyg/iris-admin/server/cache"
-	"github.com/snowlyg/iris-admin/server/viper"
 	"github.com/snowlyg/iris-admin/server/web"
-	"github.com/snowlyg/iris-admin/server/zap"
 )
 
 var Version = "2.0"
 
 func main() {
-	viper.Init()
-	zap.Init()
-	cache.Init()
 	webServer := web.Init()
 	webServer.Run()
 }
