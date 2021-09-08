d5d1f3c19552cf13c26378ad1a97c851e9a96e33
snwolyg <569616226@qq.com>
2021-09-08 21:03:57 +0800

    增加静态页面加载

--- example/single_with_vue/main.go
+++ example/single_with_vue/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"path/filepath"
-
 	"github.com/kataras/iris/v12"
-	"github.com/snowlyg/helper/dir"
 	"github.com/snowlyg/iris-admin/server/web"
 )
 
 func main() {
 	webServer := web.Init()
-	webServer.AddStatic("/admin", iris.Dir(filepath.Join(dir.GetCurrentAbPath(), "admin")))
+	templatesFS := iris.PrefixDir("./dist", AssetFile())
+	publicFS := iris.PrefixDir("./dist", AssetFile())
+	webServer.AddStatic(templatesFS, publicFS)
 	webServer.Run()
 }
