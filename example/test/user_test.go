cf12f5ecb50f1f0b0e304737b78d0ce491181ce8
snwolyg <569616226@qq.com>
2021-09-16 22:44:14 +0800

    去除静态页面

--- example/test/user_test.go
+++ example/test/user_test.go
@@ -2,18 +2,20 @@ package test
 
 import (
 	"testing"
+
+	"github.com/snowlyg/helper/tests"
 )
 
 func TestList(t *testing.T) {
 	client := TestServer.GetTestLogin(t, "/api/v1/auth/login", nil)
 	defer client.Logout("/api/v1/users/logout", nil)
 
-	// url := "v1/admin/api/getApiList"
-	// pageKeys := tests.Responses{
-	// 	{Key: "pageSize", Value: 10},
-	// 	{Key: "page", Value: 1},
-	// 	{Key: "list", Value: nil},
-	// 	{Key: "total", Value: source.BaseApisLen()},
-	// }
-	// base.PostList(auth, url, base.PageRes, http.StatusOK, "获取成功", pageKeys)
+	url := "/api/v1/users"
+	pageKeys := tests.Responses{
+		{Key: "pageSize", Value: 10},
+		{Key: "page", Value: 1},
+		{Key: "list", Value: nil},
+		{Key: "total", Value: 0, Type: "GE"},
+	}
+	client.GET(url, pageKeys, tests.RequestParams)
 }
